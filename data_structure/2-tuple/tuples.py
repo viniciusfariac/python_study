116fc1a26161c8055159b7a0f23818e93e9cc8a2
viniciusfariac <vfariacerqueira@gmail.com>
2025-11-21 14:33:53 -0300

    add = leassons about tuple

---
+++ data_structure/2-tuple/tuples.py
@@ -0,0 +1,13 @@
+frutas = ("laranja", "pera", "uva",)
+
+letras = tuple("python")
+
+numeros = tuple([1,2,3,4,5,6])
+
+pais = ("Brasil",)
+
+print(f"""
+{print(frutas[0])}
+{print(numeros[-1])}
+{print(len(numeros))}
+""")
