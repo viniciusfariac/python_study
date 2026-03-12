80831db937832c31502bbd4bf6a8a04c42e3d986
viniciusfariac <vinicius.cerqueira@sptech.school>
2026-03-12 11:37:09 -0300

    feat: exercise more simple

--- codewars/ex5.py
+++ codewars/ex5.py
@@ -1,24 +1,24 @@
 import math
+# def series_sum(n):
+#     # Happy Coding ^_^
+#     if n == 0:
+#         return "0.00"
+    
+#     valor = 1
+#     n = n * 3
+#     for i in range(1, n):
+#         div = 1 / ((1 + 3) * float(i))
+#         valor += div
+
+#     return f"{valor:.2f}"
+
 def series_sum(n):
     # Happy Coding ^_^
-    if n == 0:
-        return "0.00"
-    
-    valor = 1
-    n = n * 3
-    for i in range(1, n):
-        div = 1 / ((1 + 3) * float(i))
-        valor += div
+    valor = 0.0
+    for i in range(0, n):
+        valor += 1 / (1 + 3 * float(i))
 
     return f"{valor:.2f}"
-
-
-
-# def series_sum(n):
-#     sum = 0.0
-#     for i in range(0,n):
-#         sum += 1 / (1 + 3 * float(i))
-#     return '%.2f' % sum
 
 print(series_sum(0))
 print(series_sum(1))
