66267b30b04a65a171a951a8acc2ee6fc95bb9dd
viniciusfariac <vinicius.cerqueira@sptech.school>
2026-03-23 10:31:24 -0300

    feat: entendendo algoritmos and first book exercise

---
+++ book/entendendo_algoritmos/capitulo1/pesquisa_binaria_ex.py
@@ -0,0 +1,19 @@
+def pesquisa_binaria (lista, item):
+    baixo = 0
+    alto = len(lista) - 1
+    while baixo <= alto:
+        meio = int(((baixo + alto) / 2))
+        chute = lista[meio]
+
+        if chute == item:
+            return meio
+        if chute > item:
+            alto = meio - 1
+        else:
+            baixo = meio + 1
+    return None
+
+minha_lista = [1,2,3,4,5,6,7,8,9,10,11,12]
+
+print(pesquisa_binaria(minha_lista, 1))
+print(pesquisa_binaria(minha_lista, 11))
